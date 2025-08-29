f399dae85153938ddd92af01d6b048304e8de54f
klucio <klucio@soeffects.com>
2025-08-29 16:24:15 -0700

    Bypass conflict between bufferline and catppuccin in lazyvim

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,10 +1,20 @@
--- File containing Catppuccin themes
+-- File containing Catppuccin themes and bypass for bufferline bug due to catppuccin
 return {
+  { -- fix bug caused by catppuccin bufferline integration error
+    "akinsho/bufferline.nvim",
+    init = function()
+      local bufline = require("catppuccin.groups.integrations.bufferline")
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
+  },
+
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
-      flavour = "macchiato"
-    }
-  }
+      flavour = "macchiato",
+    },
+  },
 }
