dbc6dc147fed9528b1287605565a9beb12dd18ae
KaitlinLucio <omenmis@ophelia.yuri>
2025-10-16 01:54:44 -0700

    Fix and update for new design

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,14 +1,14 @@
 -- File containing Catppuccin themes and bypass for bufferline bug due to catppuccin
 return {
-  { -- fix bug caused by catppuccin bufferline integration error
-    "akinsho/bufferline.nvim",
-    init = function()
-      local bufline = require("catppuccin.groups.integrations.bufferline")
-      function bufline.get()
-        return bufline.get_theme()
-      end
-    end,
-  },
+  -- { -- fix bug caused by catppuccin bufferline integration error
+  --   "akinsho/bufferline.nvim",
+  --   init = function()
+  --     local bufline = require("catppuccin.groups.integrations.bufferline")
+  --     function bufline.get()
+  --       return bufline.get_theme()
+  --     end
+  --   end,
+  -- },
 
   {
     "LazyVim/LazyVim",
